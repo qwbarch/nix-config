7170a8a66e777092c9e6bef6a2ff334ca0216e0a
qwbarch <qwbarch@gmail.com>
2022-08-31 17:00:37 -0400

    Enable gaps

--- home/programs/i3-gaps/default.nix
+++ home/programs/i3-gaps/default.nix
@@ -3,10 +3,43 @@
 {
   xsession = {
     enable = true;
+
     windowManager.i3 = {
       enable = true;
       package = pkgs.i3-gaps;
+      config = rec {
+        modifier = "Mod4";
+        bars = [];
+
+	gaps = {
+          inner = 15;
+	  outer = 5;
+	};
+
+        window = {
+          border = 0;
+	  hideEdgeBorders = "both";
+	};
+
+        keybindings = lib.mkOptionDefault {
+          # Alacritty terminal
+          "${modifier}+Return" = "exec ${pkgs.alacritty}/bin/alacritty";
+
+          # Vim-inspired movement keys
+          "${modifier}+j" = "focus down";
+          "${modifier}+k" = "focus up";
+          "${modifier}+h" = "focus left";
+          "${modifier}+l" = "focus right";
+        };
+
+        startup = [
+          {
+            command = "systemctl --user restart polybar.service";
+            always = true;
+            notification = false;
+          }
+        ];
+      };
     };
   };
-  home.file.".i3/config".source = ./config;
 }
