c4ce10eced188369b9e9b0a3c31859848df258bd
qwbarch <qwbarch@gmail.com>
2022-09-04 22:33:52 -0400

    Switch vertical resize keys

--- home/programs/i3-gaps/default.nix
+++ home/programs/i3-gaps/default.nix
@@ -94,8 +94,8 @@ in {
 
         modes.resize = {
           "h" = "resize grow width 10 px or 10 ppt";
-          "j" = "resize grow height 10 px or 10 ppt";
-          "k" = "resize shrink height 10 px or 10 ppt";
+          "j" = "resize shrink height 10 px or 10 ppt";
+          "k" = "resize grow height 10 px or 10 ppt";
           "l" = "resize shrink width 10 px or 10 ppt";
           "Escape" = "mode default";
         };
