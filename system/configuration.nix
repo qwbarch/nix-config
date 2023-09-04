1906b220fa8cdac037b4971ce790b33a1efe0fb4
qwbarch <qwbarch@gmail.com>
2023-09-03 20:58:39 -0400

    Change garbage collection options

--- system/configuration.nix
+++ system/configuration.nix
@@ -125,7 +125,7 @@
         gc = {
           automatic = true;
           dates = "weekly";
-          options = "--delete-older-than 7d";
+          options = "--delete-older-than 90d";
         };
         settings = {
           auto-optimise-store = true;
